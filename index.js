85960f72feedab3364e10cc38db0bd1449bdb124
Christian Tielking <tielking5@gmail.com>
2021-08-11 17:03:36 -0400

    all cards diplay now

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { Manager, Intern, Engineer } = require('./lib/index.js');
 const generateHTML = require('./src/generateHTML');
+var manager;
+var employees = [];
+
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
@@ -11,7 +15,7 @@ function writeToFile(fileName, data) {
     })
 }
 
-function init() {
+function  managerCard() {
     return inquirer.prompt([
         {
             type: 'input',
@@ -22,16 +26,104 @@ function init() {
         {
             type: 'input',
             name: 'email',
-            message: 'What is the managers email?'
+            message: "What is the manager's email?"
         },
         {
             type: 'input',
             name: 'id',
-            message: 'What is the managers id'
+            message: "What is the manager's id?"
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is the manager's office number?"
         }
     ])
     .then(employeeData => {
-        const content = generateHTML(employeeData);
-        writeToFile('./README.md', content);
+        manager = new Manager(employeeData.name, employeeData.id, employeeData.email, employeeData.office)
+        init();
     })
 }
+
+function internCard() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the intern's id?"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What school is the intern from?"
+        }
+    ])
+    .then(employeeData => {
+        employees.push(new Intern(employeeData.name, employeeData.id, employeeData.email, employeeData.school))
+        init();
+    })
+}
+
+function  engineerCard() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?"
+
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engieer's id?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub username?"
+        }
+    ])
+    .then(employeeData => {
+        employees.push(new Engineer(employeeData.name, employeeData.id, employeeData.email, employeeData.github))
+        init();
+    })
+}
+
+function init() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Would you like to add an Intern or Engineer? Choose DONE when you are finished.',
+            choices: ['Intern', 'Engineer', 'DONE']
+        }
+    ])
+    .then(employeeData => {
+        if (employeeData.choice === 'Intern') {
+            internCard();
+        } else if (employeeData.choice === 'Engineer') {
+            engineerCard();
+        } else {
+            const content = generateHTML(manager, employees);
+            writeToFile('./dist/index.html', content)
+            console.log('Thank you!')
+        }
+    })
+}
+
+managerCard();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,19 +1,78 @@
-function generateHTML(data) {
-    function renderManager () {
-        const manCard = `
-        <div class="card">
-        <div class="card-header bg-primary">
-            ${data.name}<br>
-            <span class="oi oi-briefcase"></span> Manager
-        </div>
-        <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item border">ID: ${data.id}</li>
-                <li class="list-group-item border">Email: tielking5@gmail.com</li>
-                <li class="list-group-item border">Office number: 1</li>
-            </ul>
-        </div>
-        `
-        return manCard
-    }
+var Card = ``; 
+function renderEmployees (data) {
+    for (var i = 0; i < data.length; i++){
+        if (data[i].getRole() === 'Intern') {
+            Card = Card + `
+            <div class="card">
+            <div class="card-header bg-primary">
+                ${data[i].getName()}<br>
+                <span class="oi oi-book"></span> Intern
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item border">ID: ${data[i].getID()}</li>
+                    <li class="list-group-item border">Email: <a href="mailto:${data[i].getEmail()}">${data[i].getEmail()}</a></li>
+                    <li class="list-group-item border">School: ${data[i].getSchool()}</li>
+                </ul>
+            </div>
+            </div>
+            `
+        } else {
+            Card = Card + `
+            <div class="card">
+            <div class="card-header bg-primary">
+                ${data[i].getName()}<br>
+                <span class="oi oi-laptop"></span> Engineer
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item border">ID: ${data[i].getID()}</li>
+                    <li class="list-group-item border">Email: <a href="mailto:${data[i].getEmail()}">${data[i].getEmail()}</a></li>
+                    <li class="list-group-item border">GitHub: <a href="https://www.github.com/${data[i].getGithub()}">${data[i].getGithub()}</a></li>
+                </ul>
+            </div>
+            </div>
+            `
+        }
+    }    
+    return Card
 }
+
+function generateHTML (manager, employees) {
+    const manCard = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="bg-danger">My Team</header>
+    <div class="main d-flex flex-wrap justify-content-evenly">
+    <div class="card">
+    <div class="card-header bg-primary">
+        ${manager.getName()}<br>
+        <span class="oi oi-briefcase"></span> Manager
+    </div>
+    <div class="card-body">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item border">ID: ${manager.getID()}</li>
+            <li class="list-group-item border">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item border">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+    </div>
+    ${renderEmployees(employees)}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
+    `
+    return manCard
+}
+
+module.exports = generateHTML;
